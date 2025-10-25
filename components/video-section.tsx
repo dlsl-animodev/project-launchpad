097b403e5ff22d5e437ed1ef82c89799060dded2
Kent0710 <https://github.com/Eduard-K-A/temp-project-launchpad>
2025-10-25 11:41:50 +0800

    feat(frontend): add video section responsive design

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -7,9 +7,9 @@ import { X } from "lucide-react";
 // scrolling animation on scale and y axis
 const VideoSection = () => {
     return (
-        <div className="mt-[10rem] space-y-[5rem] px-[1rem]">
+        <div className="flex flex-wrap lg:flex-nowrap lg:items-center mt-[10rem] space-y-[5rem] px-[1rem]">
             <motion.h2
-                className="font-bold font-bebas text-7xl text-center"
+                className="font-bold font-bebas text-7xl lg:text-8xl text-center lg:flex-1/2"
                 initial={{ opacity: 0, y: 20, scale: 0.7 }}
                 whileInView={{ opacity: 1, y: 0, scale: 1 }}
                 transition={{ duration: 0.1 }}
@@ -17,56 +17,58 @@ const VideoSection = () => {
                 JOIN THE FUN, CREATIVITY, AND INNOVATION!
             </motion.h2>
 
-            <motion.section
-                className="relative bg-secondary w-full h-[25rem] p-4 pt-12 border-retro origin-center"
-                initial={{ scale: 0, y: 40 }}
-                whileInView={{ scale: 1, y: 0 }}
-                transition={{
-                    type: "spring",
-                    stiffness: 90,
-                    damping: 15,
-                }}
-            >
-                {/* Put scale animation only on this inner div */}
-                <motion.div
-                    initial={{ scale: 0 }}
-                    whileInView={{ scale: 1 }}
+            <div className="flex-1 lg:flex-1/2 flex justify-center">
+                <motion.section
+                    className="relative bg-secondary aspect-square p-4 pt-12 border-retro"
+                    initial={{ scale: 0, y: 40 }}
+                    whileInView={{ scale: 1, y: 0 }}
                     transition={{
                         type: "spring",
                         stiffness: 90,
                         damping: 15,
                     }}
-                    className="relative w-full h-full"
                 >
-                    <video
-                        src="/sample-video.mp4"
-                        className="w-full h-full object-cover border-retro"
-                        autoPlay
-                        loop
-                        muted
-                    />
-                </motion.div>
+                    {/* Put scale animation only on this inner div */}
+                    <motion.div
+                        initial={{ scale: 0 }}
+                        whileInView={{ scale: 1 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 90,
+                            damping: 15,
+                        }}
+                        className="relative w-full h-full"
+                    >
+                        <video
+                            src="/sample-video.mp4"
+                            className="w-full h-full object-cover border-retro"
+                            autoPlay
+                            loop
+                            muted
+                        />
+                    </motion.div>
 
-                <div className="absolute top-3 right-4 z-50 pointer-events-none">
-                    <div className="flex gap-2 items-center">
-                        <Minus
-                            className="bg-accent border-retro"
-                            size={27}
-                            color="white"
-                        />
-                        <Square
-                            className="bg-accent border-retro p-1"
-                            size={27}
-                            color="white"
-                        />
-                        <X
-                            className="bg-accent border-retro"
-                            size={27}
-                            color="white"
-                        />
+                    <div className="absolute top-3 right-4 z-50 pointer-events-none">
+                        <div className="flex gap-2 items-center">
+                            <Minus
+                                className="bg-accent border-retro"
+                                size={27}
+                                color="white"
+                            />
+                            <Square
+                                className="bg-accent border-retro p-1"
+                                size={27}
+                                color="white"
+                            />
+                            <X
+                                className="bg-accent border-retro"
+                                size={27}
+                                color="white"
+                            />
+                        </div>
                     </div>
-                </div>
-            </motion.section>
+                </motion.section>
+            </div>
         </div>
     );
 };
