58a369b578ba63c2ae23f1498795c293b247c0b7
Kent0710 <christian_kent_bayani@dlsl.edu.ph>
2025-11-28 09:29:59 +0800

    fix(style): line height and padding of the video section

--- components/video-section.tsx
+++ components/video-section.tsx
@@ -9,7 +9,7 @@ const VideoSection = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 lg:items-center mt-[10rem] space-y-[2rem] lg:space-y-[5rem] px-[1rem]">
             <SectionTitle
-                className="lg:flex-1/2 leading-[1.1]"
+                className="lg:flex-1/2 leading-[1.2] sm:leading-[1.1]"
                 initial={{ opacity: 0, y: 20, scale: 0.7 }}
                 whileInView={{ opacity: 1, y: 0, scale: 1 }}
                 transition={{ duration: 0.2, delay: 0.2 }}
@@ -22,7 +22,7 @@ const VideoSection = () => {
 
             <div className="flex-1 lg:flex-1/2 flex justify-center">
                 <motion.section
-                    className="relative bg-secondary aspect-square p-4 pt-12 border-retro"
+                    className="relative bg-secondary aspect-square p-1 pt-9 border-retro"
                     initial={{ scale: 0, y: 30, opacity: 0.8 }}
                     whileInView={{ scale: 1, y: 0, opacity: 1 }}
                     transition={{
@@ -46,7 +46,7 @@ const VideoSection = () => {
                         />
                     </div>
 
-                    <div className="absolute top-3 right-4 z-40 pointer-events-none">
+                    <div className="absolute top-1 right-1 z-40 pointer-events-none">
                         <div className="flex gap-2 items-center">
                             <Minus
                                 className="bg-primary border-retro"
